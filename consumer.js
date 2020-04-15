02575d86f781dd2bbe578b410d78e888c0257a60
Nick Mudge <nick@perfectabstractions.com>
2020-04-15 03:48:56 -0400

    Removed unneeded files.

--- consumer.js
+++
@@ -1,9 +0,0 @@
-//const features = require('./data/features');
-import features from "./data/features";
-import lite from "./dist/unpacker";
-//const lite = require('./dist/unpacker');
-
-Object.keys(features).forEach(function (key) {
-  const feat = features[key];
-  console.log(key, lite.feature(feat));
-});
